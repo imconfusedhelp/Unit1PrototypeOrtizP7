e42b1c6ce27fa41b2a09920a76216b291e498c69
imconfusedhelp <demskillzdough721129@gmail.com>
2023-02-17 15:24:28 -0800

    Update 02/17/23 3:24pm
    Completed the Bonus Features and recorded

--- Unit1PrototypeOritz/Assets/Scripts/FollowPlayer.cs
+++ Unit1PrototypeOritz/Assets/Scripts/FollowPlayer.cs
@@ -6,6 +6,7 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset = new Vector3(0, 5, -7);
+    private bool cameraView = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,18 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        if (cameraView == false && Input.GetKeyDown(KeyCode.Space))
+        {
+            cameraView = true;
+            offset = new Vector3(0, 5, -10);
+        }
+        else if (cameraView == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            cameraView = false;
+            offset = new Vector3(0, 3, 2);
+        }
+
+        // set offset on player
         transform.position = player.transform.position + offset;
     }
 }
